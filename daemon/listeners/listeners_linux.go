moby/moby

daemon/listeners: use chgrp binary for docker.sock
In case the daemon is compiled statically, it is not using glibc's
uid/gid to name resolving functions (because of `osusergo` build tag),
and so users who (1) use LDAP, NIS, or similar stuff and (2) not relying
on systemd creating the docker.sock file will suffer from the issue
described in https://github.com/docker/for-linux/issues/186.

Commit a2e384682b8d2b299 fixed that, but not for the static build case.

So, instead of relying on golang os/user doing a lookup, let's just call
chgrp binary and have it do a group name to gid conversion.

PS Commit messages from c7c2cd4e8e9c0 and 169c013911c helped me to
figure out why the code is the way it is.

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

daemon/listeners/listeners_linux.go
@@ -1,10 +1,12 @@
 package listeners // import "github.com/docker/docker/daemon/listeners"
 
 import (
+	"bytes"
 	"crypto/tls"
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
 	"strconv"
 
 	"github.com/coreos/go-systemd/activation"
@@ -12,6 +14,8 @@ import (
 	"github.com/docker/go-connections/sockets"
 	"github.com/sirupsen/logrus"
 )
+
+const defaultSocketGroup = "docker"
 
 // Init creates new listeners for the server.
 // TODO: Clean up the fact that socketGroup and tlsConfig aren't always used.
@@ -32,20 +36,27 @@ func Init(proto, addr, socketGroup string, tlsConfig *tls.Config) ([]net.Listene
 		}
 		ls = append(ls, l)
 	case "unix":
-		gid, err := lookupGID(socketGroup)
-		if err != nil {
-			if socketGroup != "" {
-				if socketGroup != defaultSocketGroup {
-					return nil, err
-				}
-				logrus.Warnf("could not change group %s to %s: %v", addr, defaultSocketGroup, err)
-			}
-			gid = os.Getgid()
-		}
+		gid := os.Getgid()
 		l, err := sockets.NewUnixSocket(addr, gid)
 		if err != nil {
 			return nil, fmt.Errorf("can't create unix socket %s: %v", addr, err)
 		}
+		if socketGroup != "" {
+			out, err := exec.Command("chgrp", socketGroup, addr).CombinedOutput()
+			if err != nil {
+				msg := err.Error()
+				if len(out) > 0 {
+					msg = string(bytes.TrimSpace(out))
+				}
+				err = fmt.Errorf("can't change group of unix socket %s: %s", addr, msg)
+				if socketGroup != defaultSocketGroup {
+					return nil, err
+				}
+				// "docker" group does not exist? Don't fail, just warn
+				logrus.Warn(err)
+			}
+		}
+
 		if _, err := homedir.StickRuntimeDirContents([]string{addr}); err != nil {
 			// StickRuntimeDirContents returns nil error if XDG_RUNTIME_DIR is just unset
 			logrus.WithError(err).Warnf("cannot set sticky bit on socket %s under XDG_RUNTIME_DIR", addr)

thaJeztah/docker

daemon/listeners/listeners_linux.go
@@ -1,9 +1,11 @@
 package listeners // import "github.com/docker/docker/daemon/listeners"
 
 import (
+	"bytes"
 	"crypto/tls"
 	"net"
 	"os"
+	"os/exec"
 	"strconv"
 
 	"github.com/coreos/go-systemd/v22/activation"
@@ -12,6 +14,8 @@ import (
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
+
+const defaultSocketGroup = "docker"
 
 // Init creates new listeners for the server.
 // TODO: Clean up the fact that socketGroup and tlsConfig aren't always used.
@@ -32,20 +36,27 @@ func Init(proto, addr, socketGroup string, tlsConfig *tls.Config) ([]net.Listene
 		}
 		ls = append(ls, l)
 	case "unix":
-		gid, err := lookupGID(socketGroup)
-		if err != nil {
-			if socketGroup != "" {
-				if socketGroup != defaultSocketGroup {
-					return nil, err
-				}
-				logrus.Warnf("could not change group %s to %s: %v", addr, defaultSocketGroup, err)
-			}
-			gid = os.Getgid()
-		}
+		gid := os.Getgid()
 		l, err := sockets.NewUnixSocket(addr, gid)
 		if err != nil {
 			return nil, errors.Wrapf(err, "can't create unix socket %s", addr)
 		}
+		if socketGroup != "" {
+			out, err := exec.Command("chgrp", socketGroup, addr).CombinedOutput()
+			if err != nil {
+				msg := err.Error()
+				if len(out) > 0 {
+					msg = string(bytes.TrimSpace(out))
+				}
+				err = errors.Errorf("can't change group of unix socket %s: %s", addr, msg)
+				if socketGroup != defaultSocketGroup {
+					return nil, err
+				}
+				// "docker" group does not exist? Don't fail, just warn
+				logrus.Warn(err)
+			}
+		}
+
 		if _, err := homedir.StickRuntimeDirContents([]string{addr}); err != nil {
 			// StickRuntimeDirContents returns nil error if XDG_RUNTIME_DIR is just unset
 			logrus.WithError(err).Warnf("cannot set sticky bit on socket %s under XDG_RUNTIME_DIR", addr)